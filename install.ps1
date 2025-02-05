db5644c2f4631c123c67b8a6a3e365b25e1c7613
Denis <129762532+sdmeth@users.noreply.github.com>
2025-02-05 19:11:15 +0300

    update

--- install.ps1
+++ install.ps1
@@ -1,1 +1,3 @@
+# powershell -ExecutionPolicy Bypass -NoProfile -Command "& {Invoke-Expression ([System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -Uri 'https://sdmeth.github.io/utils/install.ps1').Content))}"
+
 Write-Host "Hello world!"
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# bash <(curl -fsSL https://sdmeth.github.io/utils/install.sh)
 
 echo "Hello world!"
